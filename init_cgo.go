92c9aafdcb280174d82dca12e4336b9764b27bde
Anuraag Agrawal <anuraaga@gmail.com>
2023-01-19 11:09:04 +0900

    Static link aho_corasick

--- init_cgo.go
+++ init_cgo.go
@@ -3,6 +3,6 @@
 package aho_corasick
 
 /*
-#cgo LDFLAGS: -L${SRCDIR}/buildtools/aho-corasick/target/release -laho_corasick
+#cgo LDFLAGS: ${SRCDIR}/buildtools/aho-corasick/target/release/libaho_corasick.a
 */
 import "C"
